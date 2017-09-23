d941c3d945331fc766ad5f15a02a7b558eeac835
solver2255 <anikinkirill22@yandex.ru>
2017-09-23 13:08:35 +0500

    d

---
+++ js/main.js
@@ -0,0 +1,34 @@
+;( function() {
+	"use strict";
+
+	/* Utils */
+
+	var d = document;
+	var w = window;
+
+	//document.getElementById( arg )
+	d.gBI = function( arg ) {
+		return d.getElementById( arg );
+	}
+
+	//document.getElementsByTagName( arg )
+	d.gEBTN = function( arg ) {
+		return d.getElementsByTagName( arg );
+	}
+	
+	d.qS = function( arg ) {
+		return d.querySelector( arg );
+	}
+
+	d.qSA = function( arg ) {
+		return d.querySelectorAll( arg );
+	}
+
+	Element.prototype.qS = function( arg ) {
+		return this.querySelector( arg );
+	}
+
+	/* End Utils */
+
+	console.log( d.qS("BODY").qS("#main-menu") );
+}());
